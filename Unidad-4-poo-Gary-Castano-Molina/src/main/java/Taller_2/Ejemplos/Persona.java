c66c56f2f34dd12aab9bc83343b1cffb1042b46b
GaryCast <gjcm@hotmail.es>
2025-06-30 15:24:52 -0500

    feat: Gary Castaño Molina - Ejemplo Uso incorrecto de this en contexto static taller 2

--- Unidad-4-poo-Gary-Castano-Molina/src/main/java/Taller_2/Ejemplos/Persona.java
+++ Unidad-4-poo-Gary-Castano-Molina/src/main/java/Taller_2/Ejemplos/Persona.java
@@ -2,10 +2,8 @@ package Taller_2.Ejemplos;
 
 public class Persona {
     private String nombre;
-    public Persona(String nombre) {
-        this.nombre = nombre;// Usando this para referirse al atributo de la clase
-    }
-    public void mostrarNombre() {
-        System.out.println("Nombre: " + this.nombre); // Usando this para referirse al atributo de la clase
+
+    public static void mostrarNombre() {
+        System.out.println(this.nombre); // Error de compilación: No se puede usar `this` en un contexto estático
     }
 }
