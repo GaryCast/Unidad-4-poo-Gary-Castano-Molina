339d0582abcd9a3d206595ee2ac888b115c71d28
GaryCast <gjcm@hotmail.es>
2025-06-30 12:44:35 -0500

    feat: Gary Castaño Molina Ejercicio 3 Taller 1

---
+++ src/Taller1/Ejercicios/Estudiante.java
@@ -0,0 +1,26 @@
+package Taller1.Ejercicios;
+
+public class Estudiante {
+    String nombre;
+    int edad;
+    String curso;
+
+    // Constructor por defecto
+    public Estudiante() {
+        this.nombre = "Gary Castaño Molina";
+        this.edad = 42;
+        this.curso = "Ingeniería de Software";
+    }
+
+    // Constructor con parámetros
+    public Estudiante(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    // Constructor sobrecargado
+    public Estudiante(String nombre, int edad, String curso) {
+        this(nombre, edad);
+        this.curso = curso;
+    }
+}
