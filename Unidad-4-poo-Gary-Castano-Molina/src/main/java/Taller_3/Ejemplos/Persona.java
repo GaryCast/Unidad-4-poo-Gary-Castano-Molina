64b3c6b6ade5a12ba1e48324075e15bf7f1b98ca
GaryCast <gjcm@hotmail.es>
2025-06-30 18:19:05 -0500

    feat: Gary Castaño Molina - Eemplo 3 Error al acceder a atributos No estaticos desde un metodo static Taller 3

---
+++ Unidad-4-poo-Gary-Castano-Molina/src/main/java/Taller_3/Ejemplos/Persona.java
@@ -0,0 +1,9 @@
+package Taller_3.Ejemplos;
+
+public class Persona {
+    private String nombre;
+    public static void mostrarNombre() {
+        System.out.println("Nombre: " + nombre); // Error de compilación: No se puede acceder a 'nombre' desde un contexto
+        estático
+    }
+}
