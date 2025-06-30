02b1f20623ad3a608e879d6409a4084fe0b26e8b
GaryCast <gjcm@hotmail.es>
2025-06-30 18:45:16 -0500

    feat: Gary Castaño Molina - Ejrcicio 2 Clase matematicas con Metodos static Taller 3

---
+++ Unidad-4-poo-Gary-Castano-Molina/src/main/java/Taller_3/Ejercicios/Matematicas.java
@@ -0,0 +1,24 @@
+package Taller_3.Ejercicios;
+
+public class Matematicas {
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+
+    public static int resta(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicacion(int a, int b) {
+        return a * b;
+    }
+
+    public static double division(double a, double b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            System.out.println("Error: División entre cero.");
+            return Double.NaN; // Not a Number
+        }
+    }
+}
