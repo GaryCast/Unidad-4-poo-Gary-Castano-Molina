66c1315894f8685ce0d6c0302324adcc9e9a548b
GaryCast <gjcm@hotmail.es>
2025-06-30 19:10:17 -0500

    feat: Gary Castaño Molina - Ejrcicio 3 Correccion con metodo No static Taller 3

--- Unidad-4-poo-Gary-Castano-Molina/src/main/java/Taller_3/Ejercicios/Usuario.java
+++ Unidad-4-poo-Gary-Castano-Molina/src/main/java/Taller_3/Ejercicios/Usuario.java
@@ -3,12 +3,12 @@ package Taller_3.Ejercicios;
 public class Usuario {
     private String nombre;
 
-    public static void establecerNombre(String nuevoNombre) {
-        nombre = nuevoNombre;
+    // Metodo de instancia que sí puede modificar 'nombre'
+    public void establecerNombre(String nuevoNombre) {
+        this.nombre = nuevoNombre;
+    }
+
+    public void mostrarNombre() {
+        System.out.println("Nombre: " + nombre);
     }
 }
-/**Error: no se puede acceder a 'nombre' directamente
- El metodo establecerNombre es static, lo que significa que pertenece a la clase, no a una instancia específica.
- El atributo nombre es no estático, por lo tanto solo existe cuando hay un objeto creado.
- En un contexto estático no se puede usar directamente un atributo de instancia, porque no hay un objeto concreto al cual pertenecer. Tampoco puede usarse this.
- */
