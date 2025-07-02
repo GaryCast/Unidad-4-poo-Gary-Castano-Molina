4cb8706d2b7864be70dc2d30394d36be6597f5b1
GaryCast <gjcm@hotmail.es>
2025-07-01 22:59:27 -0500

    feat: Gary Castaño Molina - Ejemplo 3 Clase public com metodo de Utilidades Taller 7

---
+++ Unidad-4-poo-Gary-Castano-Molina/src/main/java/Taller_7/Ejemplos/Utilidades.java
@@ -0,0 +1,9 @@
+package Taller_7.Ejemplos;
+
+public class Utilidades {
+    public static void imprimirMensaje(String mensaje) {
+        System.out.println(mensaje);
+    }
+}
+
+// La clase Utilidades define un metodo público y static imprimirMensaje, que puede ser accedido desde cualquier otra clase
