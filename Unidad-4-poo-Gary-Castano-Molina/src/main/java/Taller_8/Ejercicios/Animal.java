8a35d51bfaad4769f85142b9a82f738e77eb2896
GaryCast <gjcm@hotmail.es>
2025-07-03 06:00:11 -0500

    feat: Gary Castaño Molina - Ejercicio 4 (primera parte) Uso incorrecto al Heredar de multiples Clases Taller 8

---
+++ Unidad-4-poo-Gary-Castano-Molina/src/main/java/Taller_8/Ejercicios/Animal.java
@@ -0,0 +1,9 @@
+package Taller_8.Ejercicios;
+
+class Animal {
+    public void comer() {
+        System.out.println("Animal comiendo");
+    }
+}
+
+
--- Unidad-4-poo-Gary-Castano-Molina/src/main/java/Taller_8/Ejercicios/Vehiculo.java
+++ Unidad-4-poo-Gary-Castano-Molina/src/main/java/Taller_8/Ejercicios/Vehiculo.java
@@ -1,16 +1,8 @@
 package Taller_8.Ejercicios;
 
-public class Vehiculo {
-    protected String marca;
-    protected int velocidadMaxima;
-
-    public Vehiculo(String marca, int velocidadMaxima) {
-        this.marca = marca;
-        this.velocidadMaxima = velocidadMaxima;
-    }
-
-    public void mostrarInformacionVehiculo() {
-        System.out.println("Marca: " + marca);
-        System.out.println("Velocidad Máxima: " + velocidadMaxima + " km/h");
+class Vehiculo {
+    public void arrancar() {
+        System.out.println("Vehículo arrancando");
     }
 }
+
