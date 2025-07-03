912a10002056b54849a90b78edfdb54edc3645aa
GaryCast <gjcm@hotmail.es>
2025-07-03 16:12:03 -0500

    feat: Gary Castaño Molina - Ejercicio 1 Clase Persona y Clase Empleado Taller 9

---
+++ Unidad-4-poo-Gary-Castano-Molina/src/main/java/Taller_9/Ejercicios/Empleado.java
@@ -0,0 +1,18 @@
+package Taller_9.Ejercicios;
+
+
+class Empleado extends Persona {
+    String departamento;
+
+    public Empleado(String nombre, int edad, String departamento) {
+        super(nombre, edad); // Llama al constructor de la clase padre (Persona)
+        this.departamento = departamento;
+    }
+
+    @Override
+    public void mostrarDetalles() {
+        super.mostrarDetalles(); // Llama al método mostrarDetalles de Persona
+        System.out.println("Departamento: " + departamento);
+    }
+}
+
