20a1833cc0d3ed48ba2e3f2abaec1449c7ac9ca8
GaryCast <gjcm@hotmail.es>
2025-07-03 17:45:48 -0500

    feat: Gary Castaño Molina - Ejemplo 1 Clase Empleado y Clase Gerente Taller 10

---
+++ Unidad-4-poo-Gary-Castano-Molina/src/main/java/Taller_10/Ejemplos/Gerente.java
@@ -0,0 +1,9 @@
+package Taller_10.Ejemplos;
+
+// Clase derivada
+public class Gerente extends Empleado {
+    @Override
+    public void trabajar() {
+        System.out.println("El gerente está gestionando el equipo.");
+    }
+}
