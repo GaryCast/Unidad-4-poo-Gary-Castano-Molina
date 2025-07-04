91ee83b0aad45ffa9cc05f4617c664e6221cfb99
GaryCast <gjcm@hotmail.es>
2025-07-03 22:15:17 -0500

    feat: Gary Castaño Molina - Ejercicio 3 (primera parte) Uso incorrecto de clases abstractas Taller 11

---
+++ Unidad-4-poo-Gary-Castano-Molina/src/main/java/Taller_11/Ejercicios/Instrumento.java
@@ -0,0 +1,7 @@
+package Taller_11.Ejercicios;
+
+abstract class Instrumento {
+    public void hacerSonido() {
+        System.out.println("Sonido genérico de instrumento");
+    }
+}
--- Unidad-4-poo-Gary-Castano-Molina/src/main/java/Taller_11/Ejercicios/Main.java
+++ Unidad-4-poo-Gary-Castano-Molina/src/main/java/Taller_11/Ejercicios/Main.java
@@ -1,12 +1,11 @@
 package Taller_11.Ejercicios;
 
-// Clase de prueba para mostrar los detalles con polimorfismo
+
 public class Main {
     public static void main(String[] args) {
-        Empleado e1 = new Gerente("Ana", 5000000, 1200000);
-        Empleado e2 = new Vendedor("Luis", 3000000, 800000);
-
-        e1.mostrarDetalles(); // Muestra salario con bono
-        e2.mostrarDetalles(); // Muestra salario con comisión
+        // No se puede instanciar una clase abstracta
+        Instrumento obj = new Instrumento(); //
     }
 }
+
+//  no se puede instanciar una clase abstracta
