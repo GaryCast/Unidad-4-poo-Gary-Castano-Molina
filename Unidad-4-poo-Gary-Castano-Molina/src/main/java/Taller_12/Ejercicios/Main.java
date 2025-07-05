16d33e00c34d024cfb0010bd4249b56d085835ae
GaryCast <gjcm@hotmail.es>
2025-07-05 18:01:54 -0500

    feat: Gary Castaño Molina - Ejercicio 1 Clase Main Taller 12

---
+++ Unidad-4-poo-Gary-Castano-Molina/src/main/java/Taller_12/Ejercicios/Main.java
@@ -0,0 +1,9 @@
+package Taller_12.Ejercicios;
+
+public class Main {
+    public static void main(String[] args) {
+        Ave miAve = new Ave();
+        miAve.volar();
+        miAve.cantar();
+    }
+}
