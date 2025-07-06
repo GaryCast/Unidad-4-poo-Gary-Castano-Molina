6890a8c98f7602e6978d8081ea5e0a5bd4f1478c
GaryCast <gjcm@hotmail.es>
2025-07-05 21:52:06 -0500

    feat: Gary Castaño Molina - Ejemplo 3 Uso de Polimorfismo para definir comportamientos especificos Taller 14

---
+++ Unidad-4-poo-Gary-Castano-Molina/src/main/java/Taller_14/Ejemplos/Empleado.java
@@ -0,0 +1,8 @@
+package Taller_14.Ejemplos;
+
+// Clase base
+public class Empleado {
+    public void trabajar() {
+        System.out.println("El empleado está trabajando.");
+    }
+}
---
+++ Unidad-4-poo-Gary-Castano-Molina/src/main/java/Taller_14/Ejemplos/Gerente.java
@@ -0,0 +1,9 @@
+package Taller_14.Ejemplos;
+
+// Clases derivadas
+public class Gerente extends Empleado {
+    @Override
+    public void trabajar() {
+        System.out.println("El gerente está gestionando el equipo.");
+    }
+}
