7109b47525a00fc44986dab3b96a4c9863a6f06f
GaryCast <gjcm@hotmail.es>
2025-07-05 22:40:42 -0500

    feat: Gary Castaño Molina - Ejercicio 3 (segunda parte) el metodo sobre escribe lo mismo Taller 14

--- Unidad-4-poo-Gary-Castano-Molina/src/main/java/Taller_14/Ejercicios/Estudiante.java
+++ Unidad-4-poo-Gary-Castano-Molina/src/main/java/Taller_14/Ejercicios/Estudiante.java
@@ -3,6 +3,6 @@ package Taller_14.Ejercicios;
 public class Estudiante extends Persona {
     @Override
     public void presentarse() {
-        System.out.println("Hola, soy un estudiante y estoy listo para aprender.");
+        System.out.println("Hola, soy una persona.");  // Mismo mensaje que la clase base
     }
 }
